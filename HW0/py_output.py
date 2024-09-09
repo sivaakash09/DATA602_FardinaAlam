75ca285ce1ade009aec72425c17e88f442b1b8e5
sivaakash09 <rsivaakash@gmail.com>
2024-09-08 20:18:41 -0400

    Completed Tasks

---
+++ HW0/py_output.py
@@ -0,0 +1,10 @@
+"""
+This is the sample to show python code output for 
+the assignment. We will print a pattern
+"""
+for x in range(6):
+    for y in range(0,x):
+        print("*", end = " ")
+    print(" ")
+        
+
